0586a61cbc150faf03f72f1c66596dd65ed812b2
wilfred <wilfredkorir46@gmail.com>
2022-08-16 16:40:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 15.5;
+let num2 = 4;
+
+let multiply = Math.floor(num1 * num2);
+
+let random = Math.floor(Math.random() * 10) + 2;
+
+let num3 = 22;
+let num4 = 6;
+let mod = Math.floor(num3%num4);
+
+let max = Math.max(1,3,6,7,12,14,18,20);
